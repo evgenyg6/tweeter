b35880828be80997d77627a62ba40dd9b5265d2f
evgenyg6 <evgenyg@live.com>
2017-09-20 18:31:30 +0000

    Pushing in case moving rooms destroyts my laptop

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -50,7 +50,7 @@ $(document).ready(function() {
         "created_at": 1461113796368
     }];
 
-
+    ////////////////////////////////////////////////
     function createTweetElement(tweetData) {
         var $article = $("<article>").addClass("tweet")
 
@@ -62,49 +62,54 @@ $(document).ready(function() {
         var $main = $("<main>").addClass("tweet-content")
             .append($("<p>").text(tweetData.content.text))
 
-        var $footer = $("<footer>").addClass("tweet-footer")
-            .append($("<div>").addClass("tweet-timestamp"))
-            .append($("<span>").text(tweetData.created_at))
+        var $icons = $("<div>").addClass("tweet-actions")
             .append($("<i>").addClass("fa fa-heart"))
             .append($("<i>").addClass("fa fa-flag"))
             .append($("<i>").addClass("fa fa-retweet"))
 
+        var $footer = $("<footer>").addClass("tweet-footer")
+            .append($("<div>").addClass("tweet-timestamp"))
+            .append($("<span>").text(tweetData.created_at))
+            .append($icons);
+
+
         var $combine = $article.append($header).append($main).append($footer);
 
         return $combine;
-
-        /* data.forEach(function(data, index) {
-             var newElement = $('#tweets-container').clone();
-             newElement.attr('id', "tweets-container_" + index)
-             newElement.find('.user-avatar').attr('src', data.user.avatars.small)
-             newElement.find('.user-name').text(data.user.name)
-             newElement.find('.user-handle').text(data.user.handle)
-             newElement.find('.tweet-content p').text(data.content.text)
-             newElement.find('.tweet-timestamp').text(data.created_at)
-
-             newElement.prependTo('#wrap');
-
-         })*/
     }
-
-
+    ////////////////////////////////////////////////
     function renderTweets(tweet) {
 
-        for (let x in tweet) {
-
-            let $tweet = createTweetElement(tweet[tweet.length - x - 1]);
-
+        tweet.forEach(function(eachTweet) {
+            let $tweet = createTweetElement(eachTweet);
             $('#tweets-container').append($tweet);
+        });
+        ////////////////////////////////////////////////
+        /*       for (let x in tweet) {
 
-        }
+                   let $tweet = createTweetElement(tweet[tweet.length - x - 1]);
+
+                   $('#tweets-container').append($tweet);
+
+               }*/
     }
 
     renderTweets(data);
 
+    $("form").submit(function(event) {
+        var tweetString = $("form").serialize();
+        console.log(tweetString);
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:8080/tweets',
+            data: tweetString,
+            dataType: 'json',
+            encode: true
+        })
 
-    // Test / driver code (temporary)
-    //$('#tweets-container').append($tweet); //insert $tweet content at the end of #tweets-container
-    //console.log($tweet); // to see what it looks like
-    //renderTweets(data);
+        event.preventDefault();
+
+    });
+
 
 });
