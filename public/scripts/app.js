d75c75f7ef04ace84c2374529afb02a0ea5006d1
evgenyg6 <evgenyg@live.com>
2017-09-21 01:25:19 +0000

    Compose button fully working and selecting text field

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,48 +1,4 @@
 $(document).ready(function() {
-    /*    / Test /
-        var data = [{
-            "user": {
-                "name": "Newton",
-                "avatars": {
-                    "small": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-                    "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-                    "large": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-                },
-                "handle": "@SirIsaac"
-            },
-            "content": {
-                "text": "If I have seen further it is by standing on the shoulders of giants"
-            },
-            "created_at": 1461116232227
-        }, {
-            "user": {
-                "name": "Descartes",
-                "avatars": {
-                    "small": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-                    "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-                    "large": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-                },
-                "handle": "@rd"
-            },
-            "content": {
-                "text": "Je pense , donc je suis"
-            },
-            "created_at": 1461113959088
-        }, {
-            "user": {
-                "name": "Johann G",
-                "avatars": {
-                    "small": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-                    "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-                    "large": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-                },
-                "handle": "@johann49"
-            },
-            "content": {
-                "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-            },
-            "created_at": 1461113796368
-        }];*/
     ////////////////////////////////////////////////
     function createTweetElement(tweetData) { //creates the web structure, and appends each individual element to the "article" variable
         var $article = $("<article>").addClass("tweet")
@@ -98,11 +54,8 @@ $(document).ready(function() {
             }
         });
     }
-
-
     loadTweets();
     ////////////////////////////////////////////////
-
     $("form").submit(function(event) { //serialize input into string obj NOT JSON
         event.preventDefault();
         var textArea = $("#tweet-input").val().length;
@@ -122,9 +75,15 @@ $(document).ready(function() {
                     $("tweet-input").value = "";
                     loadTweets();
                 }
-
             })
         };
     })
     ////////////////////////////////////////////////
+    $(".btn").click(function() {
+        /*$(".new-tweet").slideToggle(500, function() {*/
+        $(".container .new-tweet").slideToggle();
+        $(".container textarea").select();
+
+        /*   });*/
+    });
 });
